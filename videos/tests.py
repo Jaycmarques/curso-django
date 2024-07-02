48e9d7acee6820dd4de173b36b186dd01bfc41e4
Julio Marques <julio.jcmarques@gmail.com>
2024-07-01 22:31:55 -0300

    teste

--- videos/tests.py
+++ videos/tests.py
@@ -14,10 +14,8 @@ def teste_status_code(resp):
 
 
 def test_title_video(resp):
-    assert_contains(resp, '<h1>Video: Motivação</h1>')
+    assert_contains(resp, '<h1 class="mt-4 mb-3">Video: Motivação</h1>')
 
 
 def test_conteudo_video(resp):
     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/973584504?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Hotmart Club - 12- Como criar e exportar um Carrossel para Instagram usando Artboards - Google Chrome 2021-05-01 19-02-13"></iframe>')
-
-    # <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/973584504?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Hotmart Club - 12- Como criar e exportar um Carrossel para Instagram usando Artboards - Google Chrome 2021-05-01 19-02-13"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
