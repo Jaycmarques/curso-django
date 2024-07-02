f50cca7dc5f010c823d5afd2dab0ab416dfcd911
Julio Marques <julio.jcmarques@gmail.com>
2024-07-01 22:54:11 -0300

    ajuste teste conteudo

--- videos/tests.py
+++ videos/tests.py
@@ -9,13 +9,14 @@ def resp(client):
     return client.get(reverse('videos:video', args=('motivacao',)))
 
 
-def teste_status_code(resp):
+def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_title_video(resp):
+def test_title_videos(resp):
     assert_contains(resp, '<h1 class="mt-4 mb-3">Video: Motivação</h1>')
 
 
-def test_conteudo_video(resp):
-    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/973584504?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Hotmart Club - 12- Como criar e exportar um Carrossel para Instagram usando Artboards - Google Chrome 2021-05-01 19-02-13"></iframe>')
+def test_content_video(resp):
+    assert_contains(
+        resp, 'src="https://player.vimeo.com/video/973584504?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"')
