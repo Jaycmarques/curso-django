dc0b550cf048df89e2a0a78415b6ecfe0f7b7731
Julio Marques <julio.jcmarques@gmail.com>
2024-07-02 22:46:46 -0300

    template context

--- videos/views.py
+++ videos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'videos/video.html')
+    video = {'titulo': 'Video: Motivação', 'vimeo_id': 973584504}
+    return render(request, 'videos/video.html', context={'video': video})
