0bf33d960ffff2dd86df788317cfb3fe4a50f52f
Julio Marques <julio.jcmarques@gmail.com>
2024-07-03 21:17:12 -0300

    implementando slug videos

--- videos/views.py
+++ videos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video: Motivação', 'vimeo_id': 973584504}
+    videos = {
+        'motivacao': {'titulo': 'Video: Motivação', 'vimeo_id': 973584504},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 1234567},
+    }
+    video = videos[slug]
     return render(request, 'videos/video.html', context={'video': video})
