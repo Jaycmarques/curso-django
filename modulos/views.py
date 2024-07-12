520bcc1caeffda7ecc895aa5dea462e22e1b1188
Julio Marques <julio.jcmarques@gmail.com>
2024-07-12 19:09:36 -0300

    implementação view aula e links

--- modulos/views.py
+++ modulos/views.py
@@ -6,3 +6,7 @@ def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas(modulo)
     return render(request, 'modulos/modulos_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+def aula(request, slug):
+    pass
